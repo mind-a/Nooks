2f753eef978d5d20a74b49d3b60f8a198f0254bf
minda <38000849+mind-a@users.noreply.github.com>
2021-01-20 22:51:04 +0900

    React Hooks useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
 import "./styles.css";
 
-export default function App() {
+//React Hooks useState활용
+const App = () => {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello {item}</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>decrement</button>
     </div>
   );
+};
+
+//React 사용
+class AppUgly extends React.Component {
+  state = {
+    item: 1
+  };
+  render() {
+    const { item } = this.state;
+    return (
+      <div className="App">
+        <h1>Hello {item}</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <button onClick={this.incrementItem}>Increment</button>
+        <button onClick={this.decrementItem}>decrement</button>
+      </div>
+    );
+  }
+  incrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item + 1
+      };
+    });
+  };
+  decrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item - 1
+      };
+    });
+  };
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<AppUgly />, rootElement);
