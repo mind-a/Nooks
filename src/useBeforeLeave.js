b0938168fc2e8d6590133950b5e091723e5a35b2
dahyeMin <sol1829@gmail.com>
2021-01-30 18:26:26 +0900

    #2.4 useBeforeLeave

---
+++ src/useBeforeLeave.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const useBeforeLeave = (onBefore) => {
+  if (typeof onBefore !== "function") {
+    return;
+  }
+  const handle = (event) => {
+    const { clientY } = event;
+    if (clientY <= 0) {
+      onBefore();
+    }
+    // console.log("leaving");
+    onBefore();
+  };
+  useEffect(() => {
+    document.addEventListener("mouseleave", handle);
+    return () => document.removeEventListener("mouseleave", handle);
+  }, []);
+};
+
+const App = () => {
+  const befForLife = () => console.log("Please Don't Leave");
+  useBeforeLeave(befForLife);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
